a88168eb3f906f35d23a157c37c82d545044c043
zsefvcx <zsefvcx@list.ru>
2023-10-02 10:12:56 +0300

    Add package login or registration: init progect

---
+++ package_login_or_registration/lib/package_login_or_registration.dart
@@ -0,0 +1,9 @@
+library package_login_or_registration;
+
+export 'src/extensions.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ package_login_or_registration/lib/src/extensions.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+///Расширение класса num
+extension NumberTypeExtensions on num {
+
+  ///виджет - вертикльный спейс с заданной высотой
+  ///add horizontal space with given height
+  Widget get h => SizedBox(
+    height: double.parse(toString()),
+  );
+
+  ///Виджет - горизонтальный спейс с заданной шириной
+  ///add vertical space with given width
+  Widget get w => SizedBox(
+    width: double.parse(toString()),
+  );
+
+}
